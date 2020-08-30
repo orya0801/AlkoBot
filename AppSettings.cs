3fa2f26951c9f46b07c2436e4e1c49f15bc557a0
Sazonov Sergey <61204028+aeterna-quinque@users.noreply.github.com>
2020-08-30 22:24:27 +0300

    +bot +commands

---
+++ AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlkoBot
+{
+    public static class AppSettings
+    {
+        //Url ссылка на бота
+        public static string Url { get; set; } = "";
+
+        //Имя бота
+        public static string Name { get; set; } = "AlkoBot";
+
+        //Токен бота
+        public static string Token { get; set; } = "";
+    }
+}
---
+++ Models/Bot/Bot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using AlkoBot.Models.Commands;
+using Telegram.Bot.Types;
+
+namespace AlkoBot.Models.Bot
+{
+    public class Bot
+    {
+        private static TelegramBotClient botClient;
+        private static List<Command> commandsList;
+
+        public static IReadOnlyList<Command> Commands => commandsList.AsReadOnly();
+
+        public static async Task<TelegramBotClient> GetBotClientAsync()
+        {
+            if (botClient != null)
+                return botClient;
+
+            commandsList = new List<Command>();
+            commandsList.Add(new HelloCommand());
+
+            botClient = new TelegramBotClient(AppSettings.Token);
+
+            Update[] updates = await botClient.GetUpdatesAsync();
+        }
+    }
+}
